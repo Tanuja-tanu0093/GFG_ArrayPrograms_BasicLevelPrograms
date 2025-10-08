f096f2d005faffb0790a325b92665759b6cdb6a3
Tanuja-tanu0093 <rambatanuja111@gmail.com>
2025-10-08 21:01:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Elements of Array/print-elements-of-array.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    // Just print the space seperated array elements
+    void printArray(int arr[]) {
+        for(int temp:arr)
+        {
+       System.out.print(temp+" ");
+        
+    }
+}
+}
